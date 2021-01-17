38458da83833c5ecda534bf35b3cfad0d8bea536
Jon Hill <jondhill1984@gmail.com>
2021-01-17 13:47:48 +0200

    Add holder message

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,44 @@
 import * as React from "react";
 import styled from "styled-components";
+import SEO from "../components/SEO";
 
-const ContactPageStyles = styled.div``;
+const ContactPageStyles = styled.div`
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  font-size: 2rem;
+  .messageContainer {
+    width: 60%;
+  }
+`;
 
-export default function ContactPage() {
+export default function ContactPage({ data }) {
   return (
     <>
-      {/* <SEO title="Home" /> */}
+      <SEO title="Contact" />
       <ContactPageStyles>
-        {/* <div className="indexPageFooter">
-          <Footer />
-        </div> */}
-        hello - I will be the contact page
+        <div className="messageContainer">{data.message.nodes[0].Message}</div>
+        {/* <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form> */}
       </ContactPageStyles>
     </>
   );
 }
+
+export const query = graphql`
+  query messageQuery {
+    message: allSanityContactMessage {
+      nodes {
+        Message
+      }
+    }
+  }
+`;
